03c00731167543b4d33ac9667d509cc3d669cc91
wangjunling <896766857@qq.com>
2017-02-10 21:13:44 +0800

    我的圆形ImageView。

--- app/src/main/java/circleimageview/myself/com/testmycircleimageview/MainActivity.java
+++ app/src/main/java/circleimageview/myself/com/testmycircleimageview/MainActivity.java
@@ -1,7 +1,10 @@
 package circleimageview.myself.com.testmycircleimageview;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import circleimageview.myself.com.mylibrary.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleImageView circleImageView=(CircleImageView) findViewById(R.id.CircleImageView);
+        circleImageView.setOutCircleWidth(200);
+        circleImageView.setOutCircleColor(Color.WHITE);
+
+
     }
 }
